aaff20c00091830f8bf38b906b369b8874ab6a39
Lautaro Gonzalez <59624470+Lautaro124@users.noreply.github.com>
2021-11-15 15:31:05 -0300

    Routes and function

---
+++ src/routes/food.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response, NextFunction, Application } from 'express'
+
+const route: Application = express()
+
+route.get('/', (req: Request, res: Response, next: NextFunction) => {
+
+  
+})
