136f982061e8184432b754eeb64ff58e44c59e06
Juan Gatti <juanse10028@gmail.com>
2021-11-15 05:08:10 -0300

    Server connected

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,33 @@
 import express, { Application } from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import { connect } from 'mongoose'
+dotenv.config()
+
+async function Conexion(): Promise<void> {
+    try {
+        const url: string | undefined = process.env.DB_URL
+
+        await connect(`${url}`)
+        console.log('Conexion a la base de datos establecida')
+    } 
+    catch (error) {
+        console.log(error)
+    }
+}
+
+
 
 
 export default function App(port: number) {
 
+
     const app: Application = express()
 
+    Conexion()
     app.set('port', port || 3001)
+
+    app.use(morgan('dev'))
     app.listen(app.get('port'), () => {
         console.log(`Server is running on port ${app.get('port')}`)
     })
