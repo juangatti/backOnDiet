e8fccd5e9518373608d4fa940a25fbd11cd9dfad
juangatti <Juanse10028@gmail.com>
2021-11-15 17:07:16 -0300

    Add route user

--- src/routes/functions.ts
+++ src/routes/functions.ts
@@ -1,10 +1,25 @@
-import { FoodModel } from '../models/models'
+import { FoodModel, UserModel } from '../models/models'
+
 
 interface foodContent {
   _id: string;
   Name: string;
   Description: string;
 }
+
+interface userContent {
+  _id: string;
+  admin: boolean;
+  firstName: string;
+  lastName: string;
+  mail: string;
+  password: string;
+  phone: string;
+  adress: string;
+}
+
+
+
 /// Function get all foods
 export async function getFood(): Promise <any> {
   try{
@@ -32,4 +47,18 @@ export async function postFood(Name: string, Description: string) : Promise <voi
   catch(err: any){
     console.log(err)
   }
+}
+
+
+/// Function get users  
+export async function getUser(): Promise <any> {
+  try{  
+    const data: Array<userContent> = await UserModel.find().lean()
+
+    return data
+
+  }catch(err: any){
+    
+    return console.log(err)
+  } 
 }
