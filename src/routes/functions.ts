7556982ed56c5329ef5f507c65d23cb32df0b9b1
juangatti <Juanse10028@gmail.com>
2021-11-15 16:52:10 -0300

    add merge

--- src/routes/functions.ts
+++ src/routes/functions.ts
@@ -1,13 +1,10 @@
 import { FoodModel } from '../models/models'
 
-
-/// interfaces
 interface foodContent {
   _id: string;
   Name: string;
   Description: string;
 }
-
 /// Function get all foods
 export async function getFood(): Promise <any> {
   try{
@@ -19,7 +16,7 @@ export async function getFood(): Promise <any> {
 
   }catch(err: any){
     
-    return console.error(err)
+    return console.log(err)
   }
 }
 
